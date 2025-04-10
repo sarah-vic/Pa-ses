a82f4a0e55039cb772042a2fd0652281377ba845
Sarah <sah.winnermind@gmail.com>
2025-04-10 08:25:27 -0300

    Add files via upload

---
+++ page.js
@@ -0,0 +1,3 @@
+var paises = ["Mongólia", "Suécia", "Bolívia", "Senegal", "Croácia", "Brasil", "Tailândia", "Escócia", "Coreia do Sul", "Uzbequistão"]
+var paisEscolhido = parseInt(prompt("Digite um número de 0 a 9"))
+document.write("País Escolhido:   " + paises[paisEscolhido])
